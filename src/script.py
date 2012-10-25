9ee5b862800f73f6ed2f26f4333580114fe6320a
martinqt <m.ki2@laposte.net>
2012-10-25 16:36:38 +0200

    Add frequency parameter to script
    See #4

--- src/script.py
+++ src/script.py
@@ -1,11 +1,15 @@
 from functions import *
 from itertools import product
 from key import *
-import os
-import sys
+import os, sys
 sys.path.insert(0, os.getcwd()+'/src/functions')
 from file import *
 
+frequency = False
+
+for arg in sys.argv:
+	if arg == '--frequency':
+		frequency = True
 
 #the encrypted message
 input = read('input.txt')
@@ -17,7 +21,7 @@ groups = product(''.join(charList), repeat=2)
 replaceDict = getKey()
 
 #generate and write groups analysis
-doGroupsAnalysis(input, groups)
+doGroupsAnalysis(input, groups, frequency)
 
 #replacing chars using the key
 result = transform(input, replaceDict)
@@ -32,4 +36,4 @@ print(result)
 #write output
 write('output.txt', result)
 write('output.html', htmlResult)
-writeCharCount(input)
+writeCharCount(input, frequency)
