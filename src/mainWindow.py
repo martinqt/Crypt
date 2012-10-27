d36818c155fd8684622d17e71fcaebefe4741b00
martinqt <m.ki2@laposte.net>
2012-10-27 21:56:53 +0200

    Add live output

--- src/mainWindow.py
+++ src/mainWindow.py
@@ -10,10 +10,10 @@ class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
         
-        self.resize(600, 500)
+        self.resize(700, 500)
         self.setFont(QFont('Verdana')) 
         self.setWindowTitle('Crypt')
-        self.headers = ['From', 'To', 'Count/Frequency']
+        self.headers = ['From', 'To', 'Count']
         self.inputPath = 'input.txt'
         self.keyPath = 'src/key.py'
 
@@ -22,13 +22,27 @@ class MainWindow(QMainWindow):
         self.populateButton = QPushButton('Repopulate', self)
         self.populateButton.clicked.connect(self.populate)
 
+        self.frequency = QCheckBox('Frequency', self)
+        self.frequency.stateChanged.connect(self.toogleFrequency)
+
+        self.output = QTextBrowser(self)
+        self.output.setReadOnly(True)
+
         self.keyEdit = QTableView(self)
         self.keyEdit.setSortingEnabled(True)
 
         widget = QWidget(self)
         layout = QVBoxLayout()
-        layout.addWidget(self.keyEdit)
-        layout.addWidget(self.populateButton)
+        hLayout = QHBoxLayout()
+        hLayout.addWidget(self.keyEdit)
+        hLayout.addWidget(self.output)
+        layout.addLayout(hLayout)
+
+        hLayout = QHBoxLayout()
+        hLayout.addWidget(self.frequency)
+        hLayout.addWidget(self.populateButton)
+        layout.addLayout(hLayout)
+
         layout.addWidget(self.quit)
         widget.setLayout(layout)
         self.setCentralWidget(widget)
@@ -47,9 +61,17 @@ class MainWindow(QMainWindow):
         self.populate()
 
     def populate(self):
+        try:
+            self.model.itemChanged.disconnect(self.convert)
+        except RuntimeError:
+            pass
+        
         self.clearModel()
         key = getKey()
-        charCount = getCharCount(read(self.inputPath))
+        if self.frequency.checkState() == Qt.Checked:
+            charCount = getCharCount(read(self.inputPath), True)
+        else:
+            charCount = getCharCount(read(self.inputPath))
 
         for i in key:
             index = self.model.rowCount()
@@ -58,6 +80,8 @@ class MainWindow(QMainWindow):
             self.model.setItem(index, 2, QStandardItem(str(charCount[i])))
 
         self.keyEdit.sortByColumn(0, Qt.AscendingOrder)
+        self.model.itemChanged.connect(self.convert)
+        self.convert()
 
     def clearModel(self):
         self.model.clear();
@@ -71,20 +95,45 @@ class MainWindow(QMainWindow):
         self.fileMenu = self.menuBar().addMenu('File')
         self.fileMenu.addAction(self.saveAct)
 
+    def toogleFrequency(self):
+        if self.frequency.checkState() == Qt.Checked:
+            self.headers[2] = 'Frequency'
+        else:
+            self.headers[2] = 'Count'
+
+        self.model.setHorizontalHeaderLabels(self.headers)
+        self.populate()
+
+    def convert(self):
+        self.saveFile()
+        input = read(self.inputPath)
+        key = self.generateKey()
+
+        self.output.setText(transform(input, key))
+
     def saveFile(self):
         content = '''def getKey():
     return {\n'''
 
-        i = 0
-        while i < self.model.rowCount():
-            tmp = '        \''+self.escape(self.model.item(i, 0).text())+'\' : '
-            tmp += '\''+self.escape(self.model.item(i, 1).text())+'\',\n'
+        key = self.generateKey()
+        for i in key:
+            tmp = '        \''+self.escape(i)+'\' : '
+            tmp += '\''+key[i]+'\',\n'
             content += tmp
-            i += 1
 
         content += '''    }'''
 
         write('src/key.py', content)
+
+    def generateKey(self):
+        key = dict()
+
+        i = 0
+        while i < self.model.rowCount():
+            key[self.model.item(i, 0).text()] = self.model.item(i, 1).text()
+            i += 1
+
+        return key
 
     def escape(self, string):
         if string == '\'':
