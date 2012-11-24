b5506becd41ac2bb137d10bb7783231437ecf205
martinqt <m.ki2@laposte.net>
2012-11-24 15:39:32 +0100

    Improve color options

--- src/parameters.py
+++ src/parameters.py
@@ -1,3 +1,4 @@
+import configparser
 from PySide.QtCore import *
 from PySide.QtGui import *
 
@@ -8,6 +9,8 @@ class Parameters(QWidget):
         self.setFont(QFont('Verdana')) 
         self.setWindowTitle('Options - Crypt')
         self.resize(300, 150)
+
+        self.loadConfig()
 
         self.tab1 = QWidget(self)
         layout = QVBoxLayout()
@@ -43,10 +46,44 @@ class Parameters(QWidget):
 
     def convertedColorClicked(self):
         pixmap = QPixmap(16, 16)
-        pixmap.fill(QColorDialog.getColor())
-        self.convertedToolButton.setIcon(QIcon(pixmap))
+        color = QColorDialog.getColor(self.getColorFromString(self.config['HTML_COLORS']['converted']), self, 'Converted Color')
+        if color != QColor():
+            pixmap.fill(color)
+            self.config['HTML_COLORS']['converted'] = self.getStringFromColor(color)
+            self.convertedToolButton.setIcon(QIcon(pixmap))
 
     def originalColorClicked(self):
         pixmap = QPixmap(16, 16)
-        pixmap.fill(QColorDialog.getColor())
-        self.originalToolButton.setIcon(QIcon(pixmap))
+        color = QColorDialog.getColor(self.getColorFromString(self.config['HTML_COLORS']['original']), self, 'Original Color')
+        if color != QColor():
+            pixmap.fill(color)
+            self.config['HTML_COLORS']['original'] = self.getStringFromColor(color)
+            self.originalToolButton.setIcon(QIcon(pixmap))
+
+    def loadConfig(self):
+        self.config = configparser.ConfigParser()
+        self.config.read('parameters.ini')
+
+    def getColorFromString(self, string):
+        return QColor(int(string[:3]), int(string[3:6]), int(string[6:9]))
+
+    def getStringFromColor(self, color):
+        r = str(color.red())
+        if len(r) == 1:
+            r = '00'+r
+        elif len(r) == 2:
+            r = '0'+r
+
+        g = str(color.green())
+        if len(g) == 1:
+            g = '00'+g
+        elif len(g) == 2:
+            g = '0'+g
+
+        b = str(color.blue())
+        if len(b) == 1:
+            b = '00'+b
+        elif len(b) == 2:
+            b = '0'+b
+
+        return r+g+b
